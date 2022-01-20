8c88107ced350ff78727ca231c77e723cc540468
djung30 <djung30@stuy.edu>
2022-01-19 19:19:33 -0500

    updated Board

--- Board.java
+++ Board.java
@@ -3,11 +3,11 @@ public class Board {
 	public Board(){
 		int [][] grid = new int[4][4];
 	}
-	
+
 	public static void populateBoard(int[][] a){
 		a [(int) (Math.random()*3)][(int) (Math.random()*3)] = 2;
 	}
-	
+
 	public static void print1( int[][] a ){
 		for(int i = 0; i < a.length; i++){
 			for(int j = 0; j < a[i].length; j++){
@@ -18,7 +18,7 @@ public class Board {
     		}
     	System.out.println();
   	}
-  	
+
   	public static void shiftRight( int[][] a ){
   		for(int i = 0; i < 4; i++){
   			for(int j = 0; j < 3; j++){
@@ -29,10 +29,21 @@ public class Board {
   			}
   		}
   	}
-  	
+
+		public static void shiftLeft( int[][] a ){
+  		for(int i = 0; i < 4; i++){
+  			for(int j = 3; j > 0; j--){
+  				if(a[i][j+1]==0){
+  					a[i][j+1]=a[i][j];
+  					a[i][j] = 0;
+  				}
+  			}
+  		}
+  	}
+
    	public static void shiftUp( int[][] a ){
   		for(int i = 3; i > 0; i=i-1){
-  			for(int j = 0; j < 3; j++){
+  			for(int j = 0; j < 4; j++){
   				if(a[i-1][j]==0){
   					a[i-1][j]=a[i][j];
   					a[i][j] = 0;
@@ -40,7 +51,7 @@ public class Board {
   			}
   		}
   	}
-  	
+
    	public static void shiftDown( int[][] a ){
   		for(int i = 0; i < 3; i++){
   			for(int j = 0; j < 4; j++){
@@ -51,7 +62,7 @@ public class Board {
   			}
   		}
   	}
-	
+
 	public static void main(String[] args) {
 		int [][] grid = new int[4][4];
 		populateBoard(grid);
@@ -61,6 +72,8 @@ public class Board {
 		shiftDown(grid);
 		print1(grid);
 		shiftUp(grid);
+		print1(grid);
+		shiftLeft(grid);
 		print1(grid);
 	}
 }
