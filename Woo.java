be0033e44ff1ea6e8bf42c355eecba794ef27eb1
Ethan Lam <elam20@stuy.edu>
2022-01-21 14:08:14 -0500

    updated

--- Woo.java
+++ Woo.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 
 public class Woo {
-
 	private static final String acceptedInputs = "wasdWASD";
-
+	public static boolean moved;
+	public static boolean gameOver = false; 
+	public static int possibleMoves = 0; 
+	
 	public static void main(String[] args) {
 		int [][] grid = new int[4][4];
 		Board game = new Board();
@@ -15,9 +17,16 @@ public class Woo {
     		s += "\n Use your WASD keys and the enter key to move the tiles. Tiles with the same number merge into one when they touch. Add them up to reach 2048! \n";
 		s += "\n \n";
     		System.out.print( s );
-    		inputGet();
-    		System.out.println(inputGet());
+    		Board.print1(grid);
+    		while (gameOver != true){
+    			moveDoer(inputGet(), grid);
+    			Board.randomTwo(grid);
+    			Board.print1(grid);
+    			Board.noPossibleMoves(grid);
+    			System.out.print( possibleMoves );
+    		}
 	}
+	
 	public static String inputGet() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter your MOVE:");
@@ -28,5 +37,21 @@ public class Woo {
 			move = input.nextLine();
 		}
 		return move; 
+	}
+	
+	public static void moveDoer(String input, int[][] a){
+		if(input.equals("w") || input.equals("W")){
+			Board.shiftUp(a);
+		}
+		if(input.equals("a") || input.equals("A")){
+			Board.shiftLeft(a);
+		}
+		if(input.equals("s") || input.equals("S")){
+			Board.shiftDown(a);
+		}
+		if(input.equals("d") || input.equals("D")){
+			Board.shiftRight(a);
+		}
+
 	}
 }
